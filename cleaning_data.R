be33ff2d454615c78b0778a942a9fa9705af2bac
ZacharySOlsen <zs0lsenprice@gmail.com>
2025-09-06 01:13:24 -0700

    Finished most of the data cleaning. My change the normalization later.

--- cleaning_data.R
+++ cleaning_data.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(httr)
 library(ggplot2)
 library(jsonlite)
+library(lubridate)
 library(purrr)
 library(readr)
 library(stringr)
@@ -49,4 +50,48 @@ closeness_centrality = closeness_centrality |> select(repo, closeness)
 # Joining with meta data.
 main_repos_meta_data = main_repos_meta_data |> inner_join(closeness_centrality, join_by(repo))
 
-# Average commits per day.
+# Average commits per day and the mean time between commits.
+main_repos_commits = main_repos_commits |> mutate(date = ymd(date))
+main_repos_commits_2020 = main_repos_commits |> filter(date >= ymd("2020-01-01"))
+
+main_repos_commits_frequency = main_repos_commits_2020 |> arrange(repo, date) |>
+  group_by(repo) |> summarize(total_commits = n(), first_date = min(date),
+                              last_date = max(date), days_active = as.numeric(difftime(last_date, first_date, units = "days")) +1,
+                              avg_commits_per_day = total_commits / days_active,
+                              mean_days_between_commits = if (n_distinct(date) > 1) mean(diff(sort(unique(date)))) else NA_real_, 
+                              .groups = "drop")
+
+# Switching logical flags to 0 and 1.
+main_repos_meta_data = main_repos_meta_data |> mutate(across(starts_with("has_"), as.integer))
+
+# Selecting wanted columns before combining into a final table.
+main_repos_meta_data = main_repos_meta_data |> select(!c(description, topics))
+
+main_repos_commits_frequency = main_repos_commits_frequency |> select(!c(total_commits, first_date, last_date, days_active))
+
+# Final table.
+master_data_file = inner_join(main_repos_meta_data, main_repos_commits_frequency, join_by(repo))
+
+# Graphing some of the data.
+ggplot(data = master_data_file, aes(stars)) + geom_histogram()
+ggplot(data = master_data_file, aes(watchers)) + geom_histogram()
+ggplot(data = master_data_file, aes(forks)) + geom_histogram()
+ggplot(data = master_data_file, aes(contributor_num)) + geom_histogram()
+ggplot(data = master_data_file, aes(size)) + geom_histogram()
+ggplot(data = master_data_file, aes(closeness)) + geom_histogram()
+ggplot(data = master_data_file, aes(avg_commits_per_day)) + geom_histogram()
+ggplot(data = master_data_file, aes(mean_days_between_commits)) + geom_histogram()
+
+# Most have large outliers. Now I'm going to normalize the data. Currently using
+# the standard score. Not sure I want to use this given how these distributions
+# look.
+master_data_file = master_data_file |> mutate(stars = (stars - mean(stars))/(sd(stars))) |>
+  mutate(watchers = (watchers - mean(watchers))/sd(watchers)) |>
+  mutate(forks = (forks - mean(forks))/sd(forks)) |>
+  mutate(contributor_num = (contributor_num - mean(contributor_num))/sd(contributor_num)) |>
+  mutate(size = (size - mean(size))/sd(size)) |>
+  mutate(avg_commits_per_day = (avg_commits_per_day - mean(avg_commits_per_day))/sd(avg_commits_per_day)) |>
+  mutate(mean_days_between_commits = (mean_days_between_commits - mean(mean_days_between_commits))/sd(mean_days_between_commits))
+
+# Saving.
+write_csv(master_data_file, "master_data_file.csv")
